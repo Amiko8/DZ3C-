ab2b57e589e6b464e0ff1ed399e7437288b83f06
Amiko8 <119101599+Amiko8@users.noreply.github.com>
2022-12-11 09:22:42 +0300

    Add files via upload

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом. Не использовать строки
+
+Console.WriteLine("Введите пятизначное число"); 
+
+long number = Convert.ToInt64(Console.ReadLine()); 
+long temp = number; 
+long poly = 0; 
+
+while (temp > 0) 
+{ 
+    poly *= 10; 
+    poly += temp % 10; 
+    temp /= 10; 
+} 
+if (number == poly) 
+{ 
+    Console.WriteLine($"Число {number} полиндром"); 
+} 
+else 
+{ 
+    Console.WriteLine($"Число {number} не является полиндромом"); 
+}
